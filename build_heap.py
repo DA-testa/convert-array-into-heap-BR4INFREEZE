c3f2462cb430d7bb601092550bb197f8cec31afc
221RDB024 <125682199+BR4INFREEZE@users.noreply.github.com>
2023-03-19 13:42:30 +0000

    Try

--- build_heap.py
+++ build_heap.py
@@ -1,42 +1,55 @@
-# python3
+import os
 
-
-def build_heap(data):
-    swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+def bh(data,i,x,swaps):
+    while(True):
+        kp, lp = i*2+1, i*2+2
+        if max(kp,lp) < x:
+            if data[i] >= max(data[kp], data[lp]):
+                break
+            elif data[kp] > data[lp]:
+                swaps = change(data,i,kp,swaps)
+                i = kp
+            else:
+                swaps = change(data, i, lp,swaps)
+                i = lp
+        else:
+            break
+    return swaps
+def change(data,i,j,swaps):
+    data[i],data[j] = data[j],data[i]
+    swaps.append(i), swaps.append(j)
     return swaps
 
+def sorter(data,size):
+    swaps = []
+    for k in range(size-1,0,-1):
+        swaps = change(data,0,k,swaps)
+        swaps = bh(data,0,k,swaps)
+    return swaps
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
-
-
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
-    print(len(swaps))
-    for i, j in swaps:
-        print(i, j)
-
-
-if __name__ == "__main__":
-    main()
+    first_input = input()
+    if first_input.__contains__('I'):
+        n = int(input())
+        data = list(map(int, input().split()))
+        assert len(data) == n
+        sorter(data,n)
+        for a in data:
+            print(a,end='')
+    elif first_input.__contains__('F'):
+        file_name = input()
+        if file_name.__contains__('a'):
+            print("INPUT-OUTPUT ERROR")
+            return
+        file = os.path.join(os.getcwd(), 'test', file_name)
+        with open(file) as f:
+            n = int(f.readline())
+            data = list(map(int, f.readline().split()))
+            assert len(data) == n
+            sorter(data,n)
+            for a in data:
+                print(a, end='')
+    else:
+        print("INPUT-OUTPUT ERROR")
+        return
+main()
